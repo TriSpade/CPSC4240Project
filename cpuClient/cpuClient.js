eba0b061f8b090738c6f5e64e1eeac7c07e19290
Steven Nix <stevencnix@gmail.com>
2017-04-14 16:02:42 -0400

    completed client and server

--- cpuClient/cpuClient.js
+++ cpuClient/cpuClient.js
@@ -4,19 +4,18 @@ var client = mqtt.connect('mqtt://127.0.0.1');
 var fs = require('fs');
 
 var T = 5000;
-var time = setInterval(cpuTimer, T);
 var cpuMath = 0;
+var time;
 var cpuLoadString;
 
-
 function cpuTimer(){
-	var os = require("os"),
+    var os = require("os"),
     cpus = os.cpus();
     hostname = os.hostname();
 
     for(var i = 0, len = cpus.length; i < len; i++) {
-    	console.log("CPU %s:", i);
-    	var cpu = cpus[i], total = 0;
+        console.log("CPU %s:", i);
+        var cpu = cpus[i], total = 0;
         for(type in cpu.times){
             total += cpu.times[type];
         }
@@ -29,8 +28,8 @@ function cpuTimer(){
                     }
                     else{
                         cpuLoadString = cpuLoadString + "\"" + "cpu" + i + "\"" + " : " + "\"" + cpuMath + "\"" + " , ";
-					}                       
-				}                         
+                    }                       
+                }                         
                 console.log("\t", type, Math.round((100 * cpu.times[type] / total)*100)/100);
             }
     }
@@ -38,28 +37,7 @@ function cpuTimer(){
     var out = "{" + "\"" + "hostname" + "\"" + " : " + "\"" + hostname + "\"" + " , " + cpuLoadString + "\"" + "time" + "\"" + " : " + "\"" + date + "\"" + "}"
     
     console.log(out);
-	client.publish('groupproject', out);
-	/*
-	
-    cpuLoadString = date + cpuLoadString;
-    console.log("\n");
-    console.log("cpu usage:", cpuLoadString);
-    var sys = require('util')
-    var exec = require('child_process').exec;
-    var child;
-		
-    child = exec("cat /sys/class/thermal/thermal_zone0/temp", function (error, stdout, stderr) {
-        if (error !== null) {
-            console.log('exec error: ' + error);
-        } 
-        else {
-            // You must send time (X axis) and a temperature value (Y axis) 
-            var date = new Date().getTime();
-            var temp = parseFloat(stdout)/1000;
-			
-            console.log("time:%s, CPU temperature: %s", date, temp);
-        }
-    });
-    */
+    client.publish('groupproject', out);
 }
 
+time = setInterval(cpuTimer, T);
